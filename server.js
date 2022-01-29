2f9b5b02ac57eed5a22f2be0a2e3fb7f9beab244
Christofer <christoferguima@gmail.comnam>
2022-01-28 22:37:12 -0300

    added server.js to deploy to heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("Application started...");
+});
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + ".index.html");
+});
+
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
