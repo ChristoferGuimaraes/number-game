2740384d8b4c10dc0fdcee988c1af6c480d6f69f
Christofer <christoferguima@gmail.comnam>
2022-01-28 22:37:41 -0300

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log("Application started...");
